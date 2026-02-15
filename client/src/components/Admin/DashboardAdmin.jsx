9fd738324724c779942bba14b0b4569c8dfb2b34
Eyad03 <eyadzmansur@gmail.com>
2026-02-15 17:14:14 +0300

    task

--- client/src/components/Admin/DashboardAdmin.jsx
+++ client/src/components/Admin/DashboardAdmin.jsx
@@ -1,23 +1,32 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { useState } from "react"; 
 import NavBar from "../NavBar";
 import SideBar from "../SideBar";
 import style from "../../style/DashboardProvider.module.css";
+import { Outlet } from "react-router-dom";
 
 function DashboardAdmin() {
-  
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className={style.layout}>
-      {/* Navbar */}
+    
+    <div className={`${style.layout} ${!isSidebarOpen ? style.sidebarHidden : ""}`}>
       <header className={style.nav}>
         <NavBar />
       </header>
 
-      {/* Sidebar */}
+      
+      <button className={style.toggleBtn} onClick={toggleSidebar}>
+        {isSidebarOpen ? "◀" : "▶"}
+      </button>
+
       <aside className={style.sidebar}>
         <SideBar />
       </aside>
 
-      {/* Main */}
       <main className={style.main}>
         <Outlet />
       </main>
--- client/src/components/Provider/DashboardProvider.jsx
+++ client/src/components/Provider/DashboardProvider.jsx
@@ -1,27 +1,34 @@
+import { useState } from "react"; 
 import NavBar from "../NavBar";
 import SideBar from "../SideBar";
 import style from "../../style/DashboardProvider.module.css";
 import { Outlet } from "react-router-dom";
 
-
 function DashboardProvider() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className={style.layout}>
-      {/* Navbar */}
+    
+    <div className={`${style.layout} ${!isSidebarOpen ? style.sidebarHidden : ""}`}>
       <header className={style.nav}>
         <NavBar />
       </header>
 
-      {/* Sidebar */}
+      
+      <button className={style.toggleBtn} onClick={toggleSidebar}>
+        {isSidebarOpen ? "◀" : "▶"}
+      </button>
+
       <aside className={style.sidebar}>
         <SideBar />
       </aside>
 
-      {/* Main */}
       <main className={style.main}>
-
         <Outlet />
-
       </main>
     </div>
   );
